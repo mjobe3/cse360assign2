a4094bda1b68750780b394682a7ee1639bbd0c56
Matthew Jobe <mjobe3@asu.edu>
2019-10-09 15:33:11 -0700

    Initial version of AddingMachine

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,67 @@
+/**
+ * <h1>AddingMachine.java</h1>
+ * 
+* @author Matthew Jobe
+* Class ID: CSE360 - 70641
+* Assignment 2
+* 
+* This AddingMachine program performs addition and subtraction operations
+* on integers and reports the total
+*/
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * This constructor assigns total to 0
+	*/
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method returns the current total
+	*/
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method adds the parameter to the total
+	*/
+	
+	public void add (int value) {
+		
+	}
+	
+	
+	/**
+	 * This method subtracts the parameter from the total
+	*/
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method returns the history of addition/subtraction operations as a string
+	*/
+		
+	public String toString () {
+		return "";
+	}
+
+	
+	/**
+	 * To be implemented
+	*/
+	
+	public void clear() {
+	
+	}
+}
